499831deab8802d7b8d1a609f1d47e47c3845fe2
esargen <esargen@g.clemson.edu>
2020-07-10 14:00:19 -0400

    added team bios, among other doodads

--- app.js
+++ app.js
@@ -1,61 +1,108 @@
 const persons = [
+
+  {
+    name: 'Reagan Ross',
+    position: 'Co-Director',
+    description: "A senior double major in Anthropology and Food Science & Technology with an emphasis in sustainability, Reagan is passionate about the deeply rooted ways food affects our culture and how we see the world. She can also tell you all about your favrite cheeses and ice creams, and she'll top anything with tahini and sriracha. Reagan joined the Clemson Food Collective in 2018 and during the past two years, she has worked to bring local, sustainable, and equitable food to the table for CFC events and has helped to bring forth positive relationships between Clemson University and the Upstate community. This year she is over the moon to move into an Executive Director position with Co-Director Ronnie Clevenstine.",
+    image: 'static/kale.jpg',
+    email: 'raross@clemson.edu'
+  },
+  {
+    name: 'Ronnie Clevenstine',
+    position: 'Co-Director',
+    description: 'Despite being a junior in Economics with a double minor in Mathemathics and Sustainability, Ronnie spends most of her time at Clemson in the sociology or political science department researching and discussing the social dimensions of our food system. Ronnie joined the CFC Team in the fall of her freshman year, thinking that sustainable, resilient local food systems were the sustainable solution of the future, and has found herself utterly obsessed with learning more about food equity, justice, and culture. She is incredibly excited to be share the Executive Director role with Reagan Ross and cannot wait to see CFC\'s continued growth on campus. ',
+    email: 'vcleven@clemson.edu'
+  },
+  {
+    name: 'Frances Schueren',
+    position: 'Finance Director',
+    description: 'A junior in Food Science and Technology with an emphasis on sustainability and minors in plant and environmental science and chemistry, Frances is passionate about making food for everyone to enjoy, especially kids. When she isn’t making stovetop curry or chocolate banana bread, she is coordinating events, and meeting with faculty to improve CFC. She has been working with CFC for 3 years.',
+    image: 'static/bean.jpg'
+  },
   {
     name: 'Emma Sargent',
-    position: 'Designer',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    position: 'Executive Designer',
+    description: 'A junior Architecture major from Aiken, SC, Emma is most likely to be found thumbing through old cookbooks or sketching out her next ambitious dessert project. A passion for design and interest in the present-day conversation about food has landed her the sweet gig of CFC Executive Designer, as well as manager for our publication, Active Culture.',
     image: 'static/strawberry.jpg',
+    email: 'esargen@g.clemson.edu'
   },
   {
     name: 'Carson Colenbaugh',
     position: 'Media Director',
-    description: 'A sophomore Horticulture major from Kennesaw, Georgia, Carson joined the Food Collective team in 2018.  His favorite fruit is the scuppernong, and he could eat fajitas every day.  As a part of the Food Collective Executive Committee,  Carson is in charge of producing our biweekly newsletter, Inch x Inch, and handling tasks for some of the pop-up events, as well as participating in planning the annual Clemson Food Summit.',
-    image: 'static/kale.jpg'
+    description: 'A sophomore Horticulture major from Kennesaw, Georgia, Carson joined the Food Collective team in 2018. His favorite fruit is the scuppernong, and he could eat fajitas every day. As a part of the Food Collective Executive Committee, Carson is in charge of producing our biweekly newsletter, Inch x Inch, and handling tasks for some of the pop-up events, as well as participating in planning the annual Clemson Food Summit.'
   },
-  {
-    name: 'Frances Schueren',
-    position: 'Executive Director',
-    description: 'A junior in Food Science and Technology with an emphasis on sustainability and minors in plant and environmental science and chemistry, Frances is passionate about making food for everyone to enjoy, especially kids. When she isn’t making stovetop curry or chocolate banana bread, she is coordinating events, and meeting with faculty to improve CFC. She has been working with CFC for 3 years.',
-    image: 'static/bean.jpg'
-  }
 ]
 
 const profiles = document.querySelector('.row');
 
+
+
 document.addEventListener('DOMContentLoaded', () => {
-for (i=0; i<persons.length; i++) {
-  let person = persons[i];
+  for (i=0; i<persons.length; i++) {
+    let person = persons[i];
 
-  const profile = document.createElement('DIV');
-  profiles.appendChild(profile);
-  profile.classList += 'column';
+    const profile = document.createElement('DIV');
+    profiles.appendChild(profile);
+    profile.classList += 'column';
 
-  const profilepic = document.createElement('DIV');
-  profile.appendChild(profilepic);
-  profilepic.classList += 'profilepic';
+    const profilepic = document.createElement('DIV');
+    profile.appendChild(profilepic);
+    profilepic.classList += 'profilepic';
 
-  if (person.hasOwnProperty('image')) {
-    newImg = document.createElement('IMG');
-    profilepic.appendChild(newImg);
-    newImg.src = person.image;
-  }
+    if (person.hasOwnProperty('image')) {
+      newImg = document.createElement('IMG');
+      profilepic.appendChild(newImg);
+      newImg.src = person.image;
+    }
 
-  if (person.hasOwnProperty('name')) {
-    newAttr = document.createElement('H4');
-    profile.appendChild(newAttr);
-    newAttr.classList += 'name';
-    newAttr.textContent = person.name;
+    if (person.hasOwnProperty('name')) {
+      newAttr = document.createElement('H4');
+      profile.appendChild(newAttr);
+      newAttr.classList += 'name';
+      newAttr.textContent = person.name;
+    }
+    if (person.hasOwnProperty('position')) {
+      newAttr = document.createElement('H5');
+      profile.appendChild(newAttr);
+      newAttr.classList += 'position';
+      newAttr.textContent = person.position;
+    }
+    if (person.hasOwnProperty('description')) {
+      description = document.createElement('P');
+      profile.appendChild(description);
+      description.classList += 'description';
+      description.textContent = person.description;
+      if (description.clientHeight > 200) {
+        description.style.maxHeight = "200px";
+        readMore = document.createElement('p');
+        profile.appendChild(readMore);
+        readMore.classList += "readmore";
+        readMore.textContent = "read more";
+      }
+    }
+    if (person.hasOwnProperty('email')) {
+      newAttr = document.createElement('A');
+      profile.appendChild(newAttr);
+      newAttr.classList += 'emaillink';
+      newAttr.setAttribute("href", "mailto:" + person.email);
+      newAttr.innerHTML = '<i class="fa fa-envelope" aria-hidden="true"></i>';
+    }
   }
-  if (person.hasOwnProperty('position')) {
-    newAttr = document.createElement('H5');
-    profile.appendChild(newAttr);
-    newAttr.classList += 'position';
-    newAttr.textContent = person.position;
+});
+
+var description = document.querySelectorAll(".description");
+
+document.addEventListener("click", () => {
+  if (event.target.matches(".readmore")) {
+    readMore = event.target;
+    parent = event.target.parentNode;
+    description = parent.querySelector(".description");
+    if (description.style.maxHeight === "200px") {
+      readMore.textContent = "close";
+      description.style.maxHeight = "";
+    } else {
+      readMore.textContent = "read more";
+      description.style.maxHeight = "200px";
+    }
   }
-  if (person.hasOwnProperty('description')) {
-    newAttr = document.createElement('P');
-    profile.appendChild(newAttr);
-    newAttr.classList += 'description';
-    newAttr.textContent = person.description;
-  }
-}
 });
